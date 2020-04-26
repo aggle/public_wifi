390f350da5652b1058060db86901a639126e7be1
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2020-04-26 16:39:41 -0400

    some small success using TweakReg
    Giovanni's scripts helped

--- tr14_headers.py
+++ tr14_headers.py
@@ -43,7 +43,7 @@ def headers2dict(filenames):
     Each file has the same keywords in the headers
     Put them all into dictionaries
     """
-    extdict = {k:[] for k in ['pri'] + [i.lower() for i in extnames]}
+    extdict = {k: [] for k in [i.lower() for i in all_headers]}
     for ff in filenames:
         hdulist = fits.open(ff)
         # primary header
@@ -60,34 +60,60 @@ def headers2dict(filenames):
 def headerdict2dataframe(header_dicts):
     """
     Convert a list of header dictionaries into a pandas dataframe
-    Args:
+
+    Parameters
+    ----------
       header_dicts: a list of header dicts
-    Returns:
+
+    Returns
+    -------
       header_df: a single pandas dataframe containing information from all the headers
     """
     numbered_dict = dict(zip(range(len(header_dicts)), header_dicts))
     header_df = pd.DataFrame(numbered_dict).T
+    header_df.columns = [i.lower() for i in header_df.columns]
     return header_df
 
 
-def write_headers(filenames):
+def write_headers(filenames, verbose=False):
    """
-   Save the headers, as csv files that can be read into dataframes, in the folder designated at the top of the file ({0}). The file name is the extension header EXTNAME value, and 'pri' refers to the primary header. Does all the work 
+   Save the headers, as csv files that can be read into dataframes, in the folder designated at the top of the file ({0}). The file name is the extension header EXTNAME value, and 'pri' refers to the primary header. Does all the work to compile and write headers. No return value; writes a file instead
+
+   Parameters
+   ----------
+   filename : None
+     a list of filenames for FITS images you want to treat as a single dataset
+   verbose : False
+     if True, print the name of the file written
+
+   Returns
+   -------
+   Nothing
    """.format(header_path)
 
    header_dicts = headers2dict(filenames)
    header_dfs = {k: headerdict2dataframe(header_dicts[k]) for k in header_dicts}
    for k, v in header_dfs.items():
        out_name = header_path / f'{k}.csv'
-       print(out_name)
+       if verbose == True:
+           print(f'Writing {out_name}')
        v.to_csv(out_name)
+    if verbose == True:
+        print("\nFinished.")
 
 
 def load_headers(extname='pri'):
     """
     Helper function to load the right header file
-    Returns a dataframe with the right header selected (default: 'pri)
-    Special case: 'all' will return a dict with all the dataframes
+
+    Parameters
+    ----------
+    extname : pri
+      shorthand name for the extension whose dataframe you want
+
+    Returns
+    -------
+    df : a dataframe with the right header selected
     """
     # special case
     if extname == 'all':
@@ -103,3 +129,25 @@ def load_headers(extname='pri'):
         return None
     df = pd.read_csv(header_path / f'{extname.lower()}.csv', index_col=0)
     return df
+
+
+def print_columns(df):
+    """
+    Helper function to print the long list of columns in a header (in alphabetical order)
+
+    Parameters
+    ----------
+    df : None
+      dataframe whose columns you want to print
+
+    Returns
+    -------
+    Nothing
+    """
+    columns = sorted(df.columns)
+    for i in columns:
+        print(i)
+
+
+if __name__ == "__main__":
+    files = Path()
