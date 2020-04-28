0c5904cf0a86c5afbfe57a0e3e36395de2867ca3
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2020-04-28 14:12:55 -0400

    gaia alignment script finished
    ran it on full-field images

--- align_tr14_to_gaia.py
+++ align_tr14_to_gaia.py
@@ -6,6 +6,7 @@ Source location: https://spacetelescope.github.io/notebooks/notebooks/DrizzlePac
 import astropy.units as u
 import glob
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import os
 
@@ -33,16 +34,27 @@ prihdrs = TH.load_headers('pri')
 scihdrs = TH.load_headers('sci')
 
 RA, Dec = prihdrs[['RA_TARG','DEC_TARG']].mean()
+coord = SkyCoord(ra=RA, dec=Dec, unit=(u.deg, u.deg))
+radius = Quantity(6., u.arcmin)
 
 if __name__ == "__main__":
 
-    print("Querying Gaia...")
     gaia_query = Gaia.query_object_async(coordinate=coord, radius=radius)
     query_path = datapath / 'gaia.cat'
-    reduced_query = gaia_query['ra', 'dec', 'phot_g_mean_mag']
-    reduced_query.write(query_path.as_posix(), format='ascii.commented_header')
-    print(f"Gaia query written to {query_path.as_posix()}.\n")
-
+    # check if catalog exists; if it does, ask to rerun
+    run_query = "N"
+    if query_path.exists() == False:
+        run_query = "Y"
+    elif query_path.exists() == True:
+        # if it exists, ask about running
+        run_query = input(f"{query_path} exists - rerun? Y/n: ").upper()
+    if run_query == "Y":
+        print("Querying Gaia...")
+        reduced_query = gaia_query['ra', 'dec', 'phot_g_mean_mag']
+        reduced_query.write(query_path.as_posix(), format='ascii.commented_header')
+        print(f"Gaia query written to {query_path.as_posix()}.\n")
+    else:
+        print(f"Skipping Gaia query; reading catalog from existing {query_path}")
 
     subarray = input("Subarray, T or F? ")
 
@@ -51,16 +63,17 @@ if __name__ == "__main__":
     else:
         subarray = False
 
-    list_of_images = [(Path("../data/tr14/") / i).as_posix() 
+    list_of_images = [(Path("../data/my_data/") / i).as_posix() 
                       for i in prihdrs.query(f'SUBARRAY == {subarray}')['FILENAME']]
     print(f"Processing {len(list_of_images)} images...")
+
     refcat = query_path.as_posix()#'gaia.cat'
     cw = 3.5  # Set to two times the FWHM of the PSF.
     wcsname = 'Gaia'  # Specify the WCS name for this alignment
 
-    tweakreg.TweakReg(list_of_images[0],#'*flc.fits',  # Pass input images
+    tweakreg.TweakReg(list_of_images,#'*flc.fits',  # Pass input images
                       updatehdr=False,  # update header with new WCS solution
-                      imagefindcfg={'threshold':500.,'conv_width':cw},  # Detection parameters, threold varies for different data
+                      imagefindcfg={'threshold':500.,'conv_width':cw},  # Detection parameters, threshold varies for different data
                       refcat=refcat,  # Use user supplied catalog (Gaia)
                       interactive=False,
                       see2dplot=False,
@@ -73,3 +86,20 @@ if __name__ == "__main__":
                       fitgeometry='general')  # Use the 6 parameter fit
     #tweakreg.TweakReg(list_of_images,
     #                  refcat=refcat)
+
+    print("\nTransformations calculated. Now run TweakReg again to apply them.\n")
+
+    tweakreg.TweakReg(list_of_images,#'*flc.fits',  # Pass input images
+                      updatehdr=True,  # update header with new WCS solution
+                      imagefindcfg={'threshold':500.,'conv_width':cw},  # Detection parameters, threold varies for different data
+                      refcat=refcat,  # Use user supplied catalog (Gaia)
+                      interactive=False,
+                      see2dplot=False,
+                      shiftfile=True,  # Save out shift file (so we can look at shifts later)
+                      outshifts='Gaia_shifts.txt',  # name of the shift file
+                      wcsname=wcsname,  # Give our WCS a new name
+                      reusename=True,
+                      sigma=2.3,
+                      ylimit=0.2,
+                      fitgeometry='general')  # Use the 6 parameter fit
+
