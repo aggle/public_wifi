1f6ae365632ab420744984ba4722d349b626d15d
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2020-05-19 00:55:36 -0400

    made sure all paths are absolute in shared_utils

--- utils/shared_utils.py
+++ utils/shared_utils.py
@@ -4,11 +4,14 @@ Includes shared useful stuff, like path definitions
 
 from pathlib import Path
 
-# Define some useful paths here
-
+"""
+This block defines some useful paths
+"""
 # HST data files for manipulation
-data_path = Path(__file__).parent / "../../data/my_data/"
+data_path = Path(__file__).parent.absolute() / "../../data/my_data/"
 # Database tables
-table_path = Path(__file__).parent / "../../data/tables/"
+table_path = Path(__file__).parent.absolute() / "../../data/tables/"
 # Gaia catalog and source matches
-align_path = Path(__file__).parent / "../../data/align_catalog"
+align_path = Path(__file__).parent.absolute() / "../../data/align_catalog/"
+# KS2 output files
+ks2_path = Path(__file__).parent.absolute() / "../../data/ks2/"
