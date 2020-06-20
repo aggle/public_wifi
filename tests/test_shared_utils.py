cd5321272bfdfabaf162ddd492211851b0dbe0fb
Jonathan Aguilar <jonathan.aguilar@jhu.edu>
2020-06-20 16:10:06 -0400

    shared_utils now loads paths from config.ini

---
+++ tests/test_shared_utils.py
@@ -0,0 +1,17 @@
+"""
+This module contains tests for tr14.utils.shared_utils.py
+"""
+import pytest
+from pathlib import Path
+import random
+
+import tr14.utils.shared_utils as shared_utils
+
+def test_load_config_path():
+    """make sure load_config_path returns paths that exist or throws exceptions"""
+    # GIVEN:
+    # WHEN:
+    # THEN:
+    # test that the path is an absolute path
+    # test that the path is the correct absolute path? don't know how to do this
+    pass
--- tr14/utils/shared_utils.py
+++ tr14/utils/shared_utils.py
@@ -1,20 +1,57 @@
+
+
 """
 Includes shared useful stuff, like path definitions
 """
 
 from pathlib import Path
+import configparser
+
+# the paths are stored in the config file
+config_file = (Path(__file__).parent.absolute() / "../config.ini").resolve()
+config = configparser.ConfigParser()
+config.read(config_file)
 
 """
 This block defines some useful paths
 """
 # HST data files for manipulation
-data_path = (Path(__file__).parent.absolute() / "../../data/my_data/").resolve()
+data_path = (Path(config['PATHS']['DATA_PATH']).resolve())
 # Database tables
-table_path = (Path(__file__).parent.absolute() / "../../data/tables/").resolve()
+table_path = (Path(config['PATHS']['TABLE_PATH']).resolve())
 # Gaia catalog and source matches
-align_path = (Path(__file__).parent.absolute() / "../../data/align_catalog/").resolve()
+align_path = (Path(config['PATHS']['ALIGN_PATH']).resolve())
 # KS2 output files
-ks2_path = (Path(__file__).parent.absolute() / "../../data/ks2/").resolve()
+ks2_path = (Path(config['PATHS']['KS2_PATH']).resolve())
+
+
+"""
+Here's a wrapper function for loading paths from the config file
+t turns relative into absolute
+"""
+def load_config_path(key):
+    """
+    Load a path from the config file. Also handles case of key not found
+    Parameters
+    ----------
+    key : str
+      a key in the PATHS section of the config file
+    Returns
+    -------
+    path : pathlib.Path or None
+      absolute path to the target in the config file. Returns None if path does not exist.
+    """
+    key = key.upper()
+    path = Path(config['PATHS'][key]).resolve()
+    # test that the path exists
+    try:
+        assert(path.exists())
+    except AssertionError:
+        print(f"Error: {path} not found.")
+        return None
+    return path
+
+
 
 """
 Here's a helper function for loading a data file
