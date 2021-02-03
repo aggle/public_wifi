a236b55b671829d6f72ac69e5825e5b58a73d78c
Jonathan Aguilar <aguilarja@gmail.com>
2021-02-03 15:53:11 -0500

    fun with testing

--- tests/test_subtr_utils.py
+++ tests/test_subtr_utils.py
@@ -9,14 +9,19 @@ import random
 from tr14.utils import subtr_utils
 
 
-
+@pytest.mark.skip("Not used for anything right now")
 def test_subtr_utils(subtr_mgr):
     """I don't know what I'm doing so figure this out here"""
     pass
 
-def test_subtr_mgr_seq_nmf(subtr_mgr):
-    """Test sequential NMF"""
-    targ_id, targ_stamp = subtr_mgr.db.stamps_tab.iloc[100][['stamp_id', 'stamp_array']]
-    ref_stamps = subtr_mgr.get_reference_stamps(targ_id, dmag_max=1)
-    residuals, models = subtr_mgr.subtr_nmf(targ_stamp, ref_stamps)
-    assert True
+class TestNMF():
+    """Suite of tests for NMF subtraction"""
+    #self.subtr_mgr = subtr_mgr
+
+    def test_subtr_mgr_seq_nmf(self, subtr_mgr):
+        """Test sequential NMF"""
+        targ_id, targ_stamp = subtr_mgr.db.stamps_tab.iloc[100][['stamp_id', 'stamp_array']]
+        ref_stamps = subtr_mgr.get_reference_stamps(targ_id, dmag_max=1)
+        residuals, models = subtr_mgr.subtr_nmf(targ_stamp, ref_stamps)
+        #assert (residuals is not None) and (models is not None)
+        assert subtr_utils.np.stack(residuals).shape == subtr_utils.np.stack(models).shape
