4eb2a73b5eae7a3904fa4bfa22c49f6a550a9bf2
Jonathan Aguilar <aguilarja@gmail.com>
2021-02-03 15:29:12 -0500

    filled out tests and fixtures for SubtrManager
    still lots left to do

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,34 +7,27 @@ from tr14.utils import subtr_utils
 #############################
 # DBManager and Subtraction #
 #############################
+# this is probably not the best way of doing things
+_dbm_mast = db_manager.DBManager(db_manager.shared_utils.db_clean_file)
+_subset_key = ('F2', # filter ID
+              'D1', # epoch ID
+              15    # sector ID
+              )
+_dbm_sec = _dbm_mast.create_subtr_subset_db(_subset_key)
+_subtr_mgr = subtr_utils.SubtrManager(_dbm_sec, calc_corr_flag=False)
+
 @pytest.fixture()
 def dbm_mast():
     """Database Manager with the master catalog"""
-    dbm = db_manager.DBManager(db_manager.shared_utils.db_clean_file)
-
-    # testing happens here
-    yield
-
-    del dbm
+    return _dbm_mast
 
 @pytest.fixture()
-def dbm_sec(dbm_mast):
+def dbm_sec():
     """Database Manager for Epoch 1, Filter 2, Sector 15"""
-    filter_id = 'F2'
-    epoch_id = 'D1'
-    sector_id = 15
-    key = (filter_id, epoch_id, sector_id)
-    dbm_sec = dbm_mast.create_subtr_subset_db(key)
+    return _dbm_sec
 
-    yield
-
-    del dbm_sec
 
 @pytest.fixture()
-def subtr_mgr(dbm_sec):
+def subtr_mgr():
     """Subtraction Manager for the test sector"""
-    subtr_mgr = subtr_utils.SubtrManager(dbm_fe, calc_corr_flag=False)
-
-    yield
-
-    del subtr_mgr
+    return _subtr_mgr
--- tests/test_subtr_utils.py
+++ tests/test_subtr_utils.py
@@ -10,6 +10,13 @@ from tr14.utils import subtr_utils
 
 
 
-def test_subtr_utils():
+def test_subtr_utils(subtr_mgr):
     """I don't know what I'm doing so figure this out here"""
+    pass
 
+def test_subtr_mgr_seq_nmf(subtr_mgr):
+    """Test sequential NMF"""
+    targ_id, targ_stamp = subtr_mgr.db.stamps_tab.iloc[100][['stamp_id', 'stamp_array']]
+    ref_stamps = subtr_mgr.get_reference_stamps(targ_id, dmag_max=1)
+    residuals, models = subtr_mgr.subtr_nmf(targ_stamp, ref_stamps)
+    assert True
