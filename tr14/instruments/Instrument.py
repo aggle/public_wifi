d1ff14cd4fe0cb051e28b1f1be86f41b9fdcc2f2
Jonathan Aguilar <aguilarja@gmail.com>
2021-04-01 23:00:51 -0400

    instruments and filters

--- tr14/instruments/Instrument.py
+++ tr14/instruments/Instrument.py
@@ -5,9 +5,10 @@ Author: Jonathan Aguilar
 Created: 2021-03-10
 """
 
+import os
 import abc
 import yaml
-
+from collections import defaultdict
 
 class Instrument(object):
     """
@@ -18,14 +19,25 @@ class Instrument(object):
 
     def __init__(self, config_file):
         """
-        Initialize an instrument from the config file
+        Initialize an instrument from the config file. By default, assigns
+        all top-level entries from the config file as attributes.
+        Overwrite in the class-specific implementation if you need something
+        different.
         """
-        file_name = "wfc3.yaml"
         ## read in configuration file and set these static variables
         package_directory = os.path.dirname(os.path.abspath(__file__))
-        config_file = os.path.join(package_directory, file_name)
-        with open(config_file) as f:
+        full_path = os.path.join(package_directory, config_file)
+        with open(full_path) as f:
             yaml_dict = yaml.load(f, Loader=yaml.SafeLoader)
             # assign all the parts of the config file directly
             for k, v in yaml_dict.items():
                 setattr(self, k, v)
+
+        self.filters = {}
+
+    def load_filter(self, filter_file):
+        """Add a filter entry to the self.filters dict"""
+        with open(filter_file, 'r') as f:
+            filt_dict = yaml.load(f, Loader=yaml.SafeLoader)
+        name = filt_dict.pop("name")
+        self.filters[name] = filt_dict
