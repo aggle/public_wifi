a7dfb51335b81d930956c8013285660e444c9c19
Jonathan Aguilar <jaguilar@stsci.edu>
2024-12-17 11:06:34 -0500

    fixed incorrect import

--- tests/unit/test_star_dashboard.py
+++ tests/unit/test_star_dashboard.py
@@ -2,28 +2,13 @@
 import pandas as pd
 from public_wifi import starclass as sc
 from public_wifi.utils import star_dashboard as sd
+from public_wifi import catalog_processing as catproc
 from bokeh.server.server import Server
 
 catalog_file = sc.Path("~/Projects/Research/hst17167-ffp/catalogs/targets_drc.csv")
 data_folder = sc.Path("/Users/jaguilar/Projects/Research/hst17167-ffp/data/HST/")
 
-# dtypes = {
-#     'target': str,
-#     'file': str,
-#     'filter': str,
-#     'ra': float,
-#     'dec': float,
-#     'x': float,
-#     'y': float,
-#     'mag_aper': float,
-#     'e_mag_aper': float,
-#     'dist': float,
-#     'snr': float,
-# }
-# catalog = pd.read_csv(str(catalog_file), dtype=dtypes)
-# catalog['x'] = catalog['x'] - 1
-# catalog['y'] = catalog['y'] - 1
-catalog = sc.load_catalog(catalog_file, 100)
+catalog = catproc.load_catalog(catalog_file, 100)
 
 # reduce catalog for quicker testing
 targets = list(catalog['target'].unique())[:10]
@@ -34,7 +19,15 @@ catalog = catalog.query(f"target in {targets}")
 
 print("processing catalog")
 # set the SIM threshold low to make sure you have enough references
-stars = sc.process_catalog(catalog, 'target', ['filter'], data_folder, 11, sim_thresh=-1.0, scale_stamps=True)
+stars = catproc.process_catalog(
+    catalog,
+    'target',
+    ['filter'],
+    data_folder,
+    11,
+    sim_thresh=-1.0,
+    scale_stamps=True
+)
 print("displaying dashboard")
 dash = sd.all_stars_dashboard(stars, plot_size=350)
 
