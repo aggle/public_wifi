a0746b5ce5f415d436632c01ddbf60fdd17c079b
Jonathan Aguilar <jaguilar@stsci.edu>
2024-12-21 21:17:36 -0500

    get_stamp_center now takes cubes and series

--- src/public_wifi/misc.py
+++ src/public_wifi/misc.py
@@ -2,11 +2,14 @@
 Useful tools
 """
 import numpy as np
+import pandas as pd
 
-def calc_stamp_center(stamp):
+def get_stamp_center(stamp : np.ndarray | pd.Series) -> np.ndarray:
     """
-    Get the central pixel of a stamp
+    Get the central pixel of a stamp or cube
     """
-    shape = np.array(stamp.shape)
+    if isinstance(stamp, pd.Series):
+        stamp = np.stack(stamp.values)
+    shape = np.array(stamp.shape[-2:])
     center = np.floor((shape/2)).astype(int)
     return center
