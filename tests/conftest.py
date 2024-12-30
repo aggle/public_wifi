68ae5df7f9ad92b5d8c98a50a458d6f629ca73a0
Jonathan Aguilar <jaguilar@stsci.edu>
2024-12-29 23:27:28 -0500

    added fixture of stars with references assigned but no subtraction

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,6 +62,10 @@ def all_stars(catalog, data_folder):
     return stars
 
 @pytest.fixture(scope='session')
+def stars_with_references(all_stars):
+    all_stars.apply(lambda star: star.set_references(compute_similarity=True))
+
+@pytest.fixture(scope='session')
 def processed_stars(catalog, data_folder):
     stars = catproc.process_catalog(
         input_catalog=catalog,
