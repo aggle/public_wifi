64638c6befcb3b83881d6d73e8fdf7692442d329
Jonathan Aguilar <jaguilar@stsci.edu>
2025-01-24 15:18:28 -0500

    added plot_library back to the repo
    this library had been removed by accident during a previous cleanup of obsolete
    files.
    
    Also made a second dashboard script to load the drizzled library

---
+++ src/star_dashboard_script-drizzled.py
@@ -0,0 +1,56 @@
+# run with `bokeh serve test_star_dashboard.py --show` from a terminal
+import pandas as pd
+
+from public_wifi import starclass as sc
+from public_wifi.dashboard import star_dashboard as sd
+from public_wifi import catalog_processing as catproc
+from public_wifi.analysis_manager import AnalysisManager
+
+from bokeh.server.server import Server
+
+# catalog_file = sc.Path("~/Projects/Research/hst17167-ffp/catalogs/targets_drc.csv")
+# data_folder = sc.Path("~/Projects/Research/hst17167-ffp/data/HST/")
+
+catalog_file = sc.Path("/user/jaguilar/hst17167-ffp/drizzled_catalog.csv")
+data_folder = sc.Path("/user/jaguilar/hst17167-ffp/drizzled_data")
+catalog = catproc.load_catalog(catalog_file)
+
+
+# load file with list of bad references
+with open("/Users/jaguilar/Projects/Research/hst17167-ffp/ffp_tools/src/ffp_tools/bad_references.txt") as f:
+    bad_references = [i.strip() for i in f.readlines()]
+
+
+nobs = len(catalog)
+nstars = len(catalog['target'].unique())
+print(f"Processing catalog: {nobs} observations of {nstars} stars...")
+
+anamgr = AnalysisManager(
+    input_catalog = catalog,
+    star_id_column = 'target',
+    match_references_on = ['filter'],
+    data_folder = data_folder,
+    stamp_size = 13,
+    bad_references = bad_references,#['J042705.86+261520.3'],
+    scale_stamps = False,
+    center_stamps = True,
+    min_nref = 30,
+    sim_thresh = 0.5,
+    snr_thresh = 5.,
+    n_modes = 5,
+    det_kklip=10,
+    mf_width=9
+)
+
+
+
+if __name__ == "__main__":
+    print("Displaying dashboard")
+    dash = sd.all_stars_dashboard(anamgr, plot_size=350)
+    port = 5006
+    apps = {'/': dash}
+    server = Server(apps, port=port)
+    print(f'\nOpening Bokeh application on http://localhost:{port}/\n')
+    server.io_loop.add_callback(server.show, "/")
+    server.io_loop.start()
+
