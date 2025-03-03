e7472c90b6dc0b8aecb696a8fd4700120ec162ca
Jonathan Aguilar <jaguilar@stsci.edu>
2025-03-03 16:31:40 -0500

    module for mcmc psf fitting, and tests

---
+++ src/public_wifi/psf_fit.py
@@ -0,0 +1,21 @@
+"""
+Tools for fitting PSFs with MCMC methods
+"""
+from photutils import psf as pupsf
+
+def construct_epsf(
+        stars : pd.Series
+) -> return_type :
+    """
+    Docstring goes here
+
+    Parameters
+    ----------
+    define your parameters
+
+    Output
+    ------
+    Define your output
+
+    """
+    pass # insert body here
